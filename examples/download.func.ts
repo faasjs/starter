8096a9e617211f5caac89ec0b53ab315a8cf908a
Ben <ben@zfben.com>
2024-10-19 08:53:01 +0000

    refactor download function to use ReadableStream for response handling

--- examples/download.func.ts
+++ examples/download.func.ts
@@ -2,27 +2,23 @@ import { useFunc } from '@faasjs/func'
 import { createReadStream } from 'node:fs'
 
 export default useFunc(() => {
-  return async ({ event }) => {
-    event.raw.response.setHeader(
-      'Content-Disposition',
-      'attachment; filename="download.func.ts"'
-    )
-    event.raw.response.setHeader('Content-Type', 'text/plain')
-
+  return async () => {
     const stream = createReadStream(`${__dirname}/download.func.ts`)
 
-    await new Promise((resolve, reject) => {
-      stream.on('open', () => stream.pipe(event.raw.response))
-
-      stream.on('error', err => {
-        event.raw.setStatus(500)
-        event.raw.setBody('Internal Server Error')
-        reject(err)
-      })
-
-      stream.on('end', resolve)
+    return new Response(new ReadableStream({
+      async start(controller) {
+        stream
+          .on('data', (chunk) => controller.enqueue(chunk))
+          .on('error', err => {
+            controller.error(err)
+          })
+          .on('end', () => controller.close())
+      }
+    }), {
+      headers: {
+        'Content-Disposition': 'attachment; filename="download.func.ts"',
+        'Content-Type': 'text/plain'
+      }
     })
-
-    event.raw.response.end()
   }
 })
